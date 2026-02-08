f72579b5bb783851829edbc085f0d36721c8dfac
Oguzhan Kaya <qront81@gmail.com>
2026-02-08 19:30:58 +0300

    Hata düzeltmeleri yapıldı

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import json
 
-# BIST 100 EKRANI - En Büyük ve Likit Hisseler (Taşma olmasın)
+# BIST 100 EKRANI (Ana Ekran)
 BIST100_HARITASI = {
     "BANKA": ["AKBNK.IS", "GARAN.IS", "ISCTR.IS", "YKBNK.IS"],
     "SAVUNMA": ["ASELS.IS", "KORDS.IS"],
@@ -12,195 +12,27 @@ BIST100_HARITASI = {
     "PERAKENDE": ["BIMAS.IS", "MGROS.IS", "SOKM.IS"],
     "OTOMOTİV": ["FROTO.IS", "TOASO.IS", "TTRAK.IS"],
     "GIDA": ["ULKER.IS", "CCOLA.IS", "AEFES.IS"],
-    "ÇİMENTO": ["KONYA.IS", "CIMSA.IS"],
     "DEMİR-ÇELİK": ["EREGL.IS", "KRDMD.IS"],
-    "KİMYA": ["SASA.IS", "GUBRF.IS", "TUPRS.IS"],
-    "SPOR": ["GSRAY.IS", "BJKAS.IS", "FENER.IS"]
+    "KİMYA": ["SASA.IS", "GUBRF.IS", "TUPRS.IS"]
 }
 
-# TÜM HİSSELER - Modal'da seçim için
+# TÜM HİSSELER (Seçim Modalı için - Özet Liste)
 TUM_HISSELER = {
-    "BANKA": [
-        "AKBNK.IS", "ISCTR.IS", "GARAN.IS", "YKBNK.IS", "HALKB.IS", "VAKBN.IS", 
-        "ICBCT.IS", "SKBNK.IS", "ALBRK.IS", "TSKB.IS", "QNBFK.IS"
-    ],
-    
-    "SAVUNMA & HAVACILIK": [
-        "ASELS.IS", "SDTTR.IS", "KORDS.IS", "OTKAR.IS", "REEDR.IS", "KATMR.IS",
-        "THYAO.IS", "PGSUS.IS", "TAVHL.IS", "DOCO.IS", "CLEBI.IS", "RYSAS.IS"
-    ],
-    
-    "ENERJİ": [
-        "ASTOR.IS", "EUPWR.IS", "SMRTG.IS", "KONTR.IS", "ALARK.IS", "CWENE.IS", 
-        "ODAS.IS", "AKENR.IS", "AKSEN.IS", "ZOREN.IS", "AYEN.IS", "HUNER.IS", 
-        "ENJSA.IS", "AYDEM.IS", "AYES.IS", "AYGAZ.IS", "ZERGY.IS", "BIOEN.IS", 
-        "EUREN.IS"
-    ],
-    
-    "HOLDİNG": [
-        "KCHOL.IS", "SAHOL.IS", "AGHOL.IS", "DOHOL.IS", "ECZYT.IS", "BIMAS.IS", 
-        "SISE.IS", "AVHOL.IS", "IHEVA.IS", "TURSG.IS", "NTHOL.IS",
-        "EUHOL.IS", "GRTHO.IS", "IEYHO.IS", "MZHLD.IS", "TRHOL.IS"
-    ],
-    
-    "TEKNOLOJİ": [
-        "LOGO.IS", "LINK.IS", "INDES.IS", "ARENA.IS", "KAREL.IS", "NETAS.IS", 
-        "ESCOM.IS", "ALCTL.IS", "ANELE.IS", "DGATE.IS", "EDATA.IS", "FONET.IS",
-        "INFO.IS", "INVEO.IS", "INTEM.IS", "NETCD.IS", "SMART.IS"
-    ],
-    
-    "PERAKENDE": [
-        "BIMAS.IS", "MGROS.IS", "SOKM.IS", "MAVI.IS", "CRFSA.IS", "MPARK.IS", 
-        "ADESE.IS", "BIZIM.IS", "KOTON.IS"
-    ],
-    
-    "OTOMOTİV": [
-        "FROTO.IS", "TOASO.IS", "TTRAK.IS", "KLMSN.IS", "TMSN.IS", "GEREL.IS", 
-        "PARSN.IS", "ASUZU.IS", "BFREN.IS", "DITAS.IS", "OTKAR.IS"
-    ],
-    
-    "GIDA": [
-        "ULKER.IS", "TATGD.IS", "PETUN.IS", "CCOLA.IS", "BANVT.IS", "PENGD.IS", 
-        "KNFRT.IS", "AEFES.IS", "EKIZ.IS", "ERSU.IS", "PINSU.IS", "KENT.IS", 
-        "BALSU.IS", "PNSUT.IS"
-    ],
-    
-    "ÇİMENTO & İNŞAAT": [
-        "KONYA.IS", "ENJSA.IS", "EGEEN.IS", "CIMSA.IS", "GOLTS.IS", "BTCIM.IS", 
-        "BUCIM.IS", "NUHCM.IS", "AKCNS.IS", "CMENT.IS"
-    ],
-    
-    "DEMİR-ÇELİK": [
-        "EREGL.IS", "KRDMD.IS", "KRDMA.IS", "KRDMB.IS", "IZMDC.IS", "ERBOS.IS", 
-        "BURCE.IS", "CELHA.IS", "DOKTA.IS", "ISDMR.IS", "CEMTS.IS"
-    ],
-    
-    "KİMYA": [
-        "SASA.IS", "GUBRF.IS", "AKSA.IS", "PETKM.IS", "TUPRS.IS", "GOODY.IS", 
-        "ALKIM.IS", "BAGFS.IS", "BRISA.IS"
-    ],
-    
-    "TEKSTİL": [
-        "HEKTS.IS", "YUNSA.IS", "BRSAN.IS", "SKTAS.IS", "BLCYT.IS", "DAGI.IS", 
-        "MAVI.IS", "ARSAN.IS", "ATEKS.IS", "DESA.IS", "DERIM.IS", "KRSTL.IS"
-    ],
-    
-    "SPOR": [
-        "GSRAY.IS", "BJKAS.IS", "FENER.IS", "TSPOR.IS"
-    ],
-    
-    "TELEKOMÜNİKASYON": [
-        "TCELL.IS", "TTKOM.IS"
-    ],
-    
-    "SİGORTA": [
-        "AKGRT.IS", "AGESA.IS", "ANSGR.IS", "ANHYT.IS"
-    ],
-    
-    "MADENCİLİK": [
-        "PAMEL.IS", "CMENT.IS", "IZMDC.IS", "GZNMI.IS"
-    ],
-    
-    "TURİZM": [
-        "MAALT.IS", "AYCES.IS", "MARTI.IS", "PKENT.IS", "MERIT.IS", "AVTUR.IS"
-    ],
-    
-    "KAĞIT": [
-        "KARTN.IS", "SILVR.IS"
-    ],
-    
-    "MEDYA": [
-        "IHLAS.IS", "IHLGM.IS", "IHYAY.IS", "IHGZT.IS"
-    ],
-    
-    "İLAÇ": [
-        "DEVA.IS", "SELEC.IS", "MTRKS.IS"
-    ],
-    
-    "DAYANIKLI TÜKETİM": [
-        "ARCLK.IS", "VESBE.IS", "ARZUM.IS", "EMKEL.IS"
-    ],
-    
-    "KUYUMCULUK": [
-        "GLDTR.IS", "ZGOLD.IS", "ALTNY.IS"
-    ],
-    
-    "GYO": [
-        "ADGYO.IS", "AGYO.IS", "AKFGY.IS", "ALGYO.IS", "AVGYO.IS", "BEGYO.IS",
-        "DGGYO.IS", "DZGYO.IS", "EKGYO.IS", "EYGYO.IS", "GRNYO.IS", "HLGYO.IS",
-        "IDGYO.IS", "ISGYO.IS", "KGYO.IS", "KLGYO.IS", "KRGYO.IS", "KZGYO.IS",
-        "MRGYO.IS", "MSGYO.IS", "NUGYO.IS", "OZGYO.IS", "OZKGY.IS", "PAGYO.IS",
-        "RYGYO.IS", "SNGYO.IS", "TDGYO.IS", "TRGYO.IS", "VKGYO.IS", "ZGYO.IS"
-    ],
-    
-    "DİĞER": [
-        "ALCAR.IS", "ASUZU.IS", "ATLAS.IS", "BASCM.IS", "BASGZ.IS",
-        "BERA.IS", "BESLR.IS", "BEYAZ.IS", "BINHO.IS", "BLUME.IS", "BMSCH.IS",
-        "BNTAS.IS", "BORLS.IS", "BOSSA.IS", "BRKO.IS", "BRMEN.IS", "BRYAT.IS",
-        "BSOKE.IS", "BULGS.IS", "BURVA.IS", "BVSAN.IS", "CANTE.IS", "CASA.IS",
-        "CEMAS.IS", "CEMZY.IS", "CONSE.IS", "COSMO.IS", "CUSAN.IS", "CVKMD.IS",
-        "DERHL.IS", "DESPC.IS", "DIRIT.IS", "DMSAS.IS", "DNISI.IS", "DOAS.IS",
-        "DOGUB.IS", "DURDO.IS", "DURKN.IS", "DYOBY.IS", "ECILC.IS", "ECOGR.IS",
-        "EDIP.IS", "EFOR.IS", "EGPRO.IS", "EKOS.IS", "ELITE.IS", "EMNIS.IS",
-        "ENDAE.IS", "ENERY.IS", "ENSRI.IS", "ENTRA.IS", "EPLAS.IS", "ERCB.IS",
-        "ESCAR.IS", "ESEN.IS", "ETILR.IS", "ETYAT.IS", "FADE.IS", "FLAP.IS",
-        "FORMT.IS", "FORTE.IS", "FRIGO.IS", "GARFA.IS", "GEDIK.IS", "GENIL.IS",
-        "GENTS.IS", "GIPTA.IS", "GLBMD.IS", "GLCVY.IS", "GLDTR.IS", "GLRMK.IS",
-        "GMSTR.IS", "GMTAS.IS", "GOKNR.IS", "GOZDE.IS", "GRSEL.IS", "GUNDG.IS",
-        "GWIND.IS", "HATEK.IS", "HATSN.IS", "HEDEF.IS", "HOROZ.IS", "HRKET.IS",
-        "HUBVC.IS", "HURGZ.IS", "IHAAS.IS", "INGRM.IS", "INTEK.IS", "INVES.IS",
-        "ISBIR.IS", "ISGSY.IS", "ISKPL.IS", "ISMEN.IS", "ISSEN.IS", "ISYAT.IS",
-        "IZENR.IS", "IZFAS.IS", "IZINV.IS", "JANTS.IS", "KAPLM.IS", "KBORU.IS",
-        "KCAER.IS", "KIMMR.IS", "KLKIM.IS", "KLNMA.IS", "KLRHO.IS", "KLSER.IS",
-        "KLSYN.IS", "KMPUR.IS", "KOCMT.IS", "KONKA.IS", "KOPOL.IS", "KRONT.IS",
-        "KRPLS.IS", "KRTEK.IS", "KRVGD.IS", "KSTUR.IS", "KTLEV.IS", "KTSKR.IS",
-        "KUTPO.IS", "KUVVA.IS", "KUYAS.IS", "KZBGY.IS", "LIDER.IS", "LILAK.IS",
-        "LKMNH.IS", "LMKDC.IS", "LYDHO.IS", "MACKO.IS", "MAGEN.IS", "MAKIM.IS",
-        "MAKTK.IS", "MANAS.IS", "MARBL.IS", "MARKA.IS", "MARMR.IS", "MEDTR.IS",
-        "MEGMT.IS", "MEKAG.IS", "MERCN.IS", "METRO.IS", "MEYSU.IS", "MIATK.IS",
-        "MMCAS.IS", "MNDRS.IS", "MOBTL.IS", "MOGAN.IS", "MOPAS.IS", "NATEN.IS",
-        "OBASE.IS", "ODINE.IS", "ONCSM.IS", "ONRYT.IS", "ORGE.IS", "ORMA.IS",
-        "OSMEN.IS", "OYAKC.IS", "OYYAT.IS", "OZRDN.IS", "OZSUB.IS", "OZYSR.IS",
-        "PAPIL.IS", "PATEK.IS", "PASEU.IS", "PEKGY.IS", "PENTA.IS", "PLTUR.IS",
-        "PNLSN.IS", "POLHO.IS", "PRKAB.IS", "PRKME.IS", "PRZMA.IS", "QUAGR.IS",
-        "RALYH.IS", "RAYSG.IS", "RGYAS.IS", "RNPOL.IS", "RUBNS.IS", "RUZYE.IS",
-        "SAFKR.IS", "SANKO.IS", "SARKY.IS", "SAYAS.IS", "SEGYO.IS", "SEKUR.IS",
-        "SELVA.IS", "SERNT.IS", "SEYKM.IS", "SMRVA.IS", "SODSN.IS", "SOKE.IS",
-        "SONME.IS", "SRVGY.IS", "SUMAS.IS", "SUNTK.IS", "SURGY.IS", "SUWEN.IS",
-        "TABGD.IS", "TARKM.IS", "TATEN.IS", "TBORG.IS", "TCKRC.IS", "TEHOL.IS",
-        "TEKTU.IS", "TERA.IS", "TEZOL.IS", "TGSAS.IS", "TKFEN.IS", "TKNSA.IS",
-        "TLMAN.IS", "TMPOL.IS", "TNZTP.IS", "TRALT.IS", "TRCAS.IS", "TRENJ.IS",
-        "TRILC.IS", "TRMET.IS", "TUCLK.IS", "TUKAS.IS", "TUREX.IS", "TURGG.IS",
-        "UCAYM.IS", "UFUK.IS", "ULAS.IS", "ULUSE.IS", "ULUUN.IS", "UNLU.IS",
-        "USAK.IS", "VAKKO.IS", "VANGD.IS", "VBTYZ.IS", "VERTU.IS", "VERUS.IS",
-        "VESTL.IS", "VKING.IS", "VSNMD.IS", "YAPRK.IS", "YAYLA.IS", "YBTAS.IS",
-        "YEOTK.IS", "YESIL.IS", "YIGIT.IS", "YKSLN.IS", "YONGA.IS", "YYAPI.IS",
-        "YYLGD.IS", "ZEDUR.IS", "ZELOT.IS"
-    ]
+    "BANKA": ["AKBNK.IS", "ISCTR.IS", "GARAN.IS", "YKBNK.IS", "VAKBN.IS", "HALKB.IS"],
+    "ENERJİ": ["ASTOR.IS", "EUPWR.IS", "SMRTG.IS", "KONTR.IS", "ALARK.IS", "ENJSA.IS"],
+    "HOLDİNG": ["KCHOL.IS", "SAHOL.IS", "SISE.IS", "DOHOL.IS", "AGHOL.IS"],
+    "DİĞER": ["THYAO.IS", "PGSUS.IS", "ASELS.IS", "EREGL.IS", "TUPRS.IS", "BIMAS.IS", "MGROS.IS", "SASA.IS"]
 }
 
 def get_stock_data(symbol):
-    """Tek bir hisse için veri çeker"""
     try:
         t = yf.Ticker(symbol)
         h = t.history(period="2d")
-        
-        if len(h) < 2:
-            return None
+        if len(h) < 2: return None
             
         degisim = ((h['Close'].iloc[-1] - h['Close'].iloc[-2]) / h['Close'].iloc[-2]) * 100
-        
-        # Piyasa değeri
         info = t.info
-        market_cap = info.get('marketCap', 0)
-        
-        if market_cap == 0 or market_cap is None:
-            shares = info.get('sharesOutstanding', 0)
-            price = h['Close'].iloc[-1]
-            if shares and shares > 0:
-                market_cap = shares * price
-            else:
-                market_cap = 1000000
+        market_cap = info.get('marketCap') or (info.get('sharesOutstanding', 0) * h['Close'].iloc[-1])
         
         return {
             "x": symbol.replace(".IS", ""), 
@@ -210,48 +42,20 @@ def get_stock_data(symbol):
     except:
         return None
 
-def get_data():
-    # BIST 100 için veri
-    bist100_data = []
-    for sektor, semboller in BIST100_HARITASI.items():
-        sektor_verisi = {"name": sektor, "data": []}
-        for sym in semboller:
-            data = get_stock_data(sym)
-            if data:
-                sektor_verisi["data"].append(data)
-                print(f"✓ {data['x']} - %{data['y']}")
-        
-        sektor_verisi["data"].sort(key=lambda x: x.get('z', 0), reverse=True)
-        if sektor_verisi["data"]:
-            bist100_data.append(sektor_verisi)
-    
-    # TÜM HİSSELER için veri
-    tum_hisseler_data = []
-    for sektor, semboller in TUM_HISSELER.items():
-        sektor_verisi = {"name": sektor, "data": []}
-        for sym in semboller:
-            data = get_stock_data(sym)
-            if data:
-                sektor_verisi["data"].append(data)
-        
-        sektor_verisi["data"].sort(key=lambda x: x.get('z', 0), reverse=True)
-        if sektor_verisi["data"]:
-            tum_hisseler_data.append(sektor_verisi)
-    
-    # İki ayrı dosya kaydet
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(bist100_data, f, ensure_ascii=False, indent=2)
-    
-    with open('all_stocks.json', 'w', encoding='utf-8') as f:
-        json.dump(tum_hisseler_data, f, ensure_ascii=False, indent=2)
-    
-    bist100_count = sum(len(s['data']) for s in bist100_data)
-    total_count = sum(len(s['data']) for s in tum_hisseler_data)
-    
-    print(f"\n{'='*50}")
-    print(f"✓ BIST 100: {bist100_count} hisse")
-    print(f"✓ TÜM HİSSELER: {total_count} hisse")
-    print(f"{'='*50}")
+def main():
+    bist_res = []
+    for s, syms in BIST100_HARITASI.items():
+        data = [get_stock_data(sm) for sm in syms if get_stock_data(sm)]
+        if data: bist_res.append({"name": s, "data": data})
+
+    all_res = []
+    for s, syms in TUM_HISSELER.items():
+        data = [get_stock_data(sm) for sm in syms if get_stock_data(sm)]
+        if data: all_res.append({"name": s, "data": data})
+
+    with open('data.json', 'w', encoding='utf-8') as f: json.dump(bist_res, f, ensure_ascii=False)
+    with open('all_stocks.json', 'w', encoding='utf-8') as f: json.dump(all_res, f, ensure_ascii=False)
+    print("Veriler başarıyla güncellendi.")
 
 if __name__ == "__main__":
-    get_data()
+    main()
--- script.js
+++ script.js
@@ -1,73 +1,40 @@
-let allD = [],           
-    allStocks = [],      
-    sel = JSON.parse(localStorage.getItem('sel')) || [], 
-    view = 'bist100',
-    currentStock = null,
-    currentTime = '1d',
-    detailChartObj = null;
+let allData = [], allStocks = [], currentView = 'bist100';
 
 async function init() {
     try {
         const r1 = await fetch('data.json');
-        allD = await r1.json();
-        
+        allData = await r1.json();
         const r2 = await fetch('all_stocks.json');
         allStocks = await r2.json();
-        
-        if (sel.length === 0) {
-            sel = ["THYAO", "ASELS", "SASA", "AKBNK", "GARAN"];
-            localStorage.setItem('sel', JSON.stringify(sel));
-        }
-        
-        upd();
-        list();
-    } catch (e) { 
-        console.error("Veri yüklenemedi!", e); 
+        renderChart();
+    } catch (e) {
+        console.error("Dosyalar bulunamadı. Önce Python scriptini çalıştırın.");
     }
 }
 
-function upd() {
-    let f;
-    
-    if (view === 'bist100') {
-        f = allD;
-    } else {
-        f = allStocks.map(s => ({
-            name: s.name, 
-            data: s.data.filter(h => sel.includes(h.x))
-        })).filter(s => s.data.length > 0);
-    }
+function renderChart() {
+    const displayData = (currentView === 'bist100') ? allData : allStocks;
 
-    const o = {
-        series: f,
-        chart: { 
-            type: 'treemap', 
-            height: '100%', 
+    const options = {
+        series: displayData,
+        chart: {
+            type: 'treemap',
+            height: '100%',
             toolbar: { show: false },
-            background: 'transparent',
-            animations: {
-                enabled: true,
-                speed: 600
-            },
             events: {
-                dataPointSelection: function(event, chartContext, config) {
-                    const seriesIndex = config.seriesIndex;
-                    const dataPointIndex = config.dataPointIndex;
-                    const stock = f[seriesIndex].data[dataPointIndex].x;
+                dataPointSelection: (event, chartContext, config) => {
+                    const stock = displayData[config.seriesIndex].data[config.dataPointIndex].x;
                     openDetail(stock);
                 }
             }
         },
-        stroke: {
-            show: true,
-            width: 5,
-            colors: ['#0a0e27']
-        },
-        // DÜZELTME: 0 = Gri, Pozitif = Yeşil, Negatif = Kırmızı
-        colors: [({ value }) => {
-            if (value === 0) return '#6b7280';      // Gri (nötr)
-            if (value > 0) return '#16c784';        // Yeşil (pozitif)
-            return '#ea3943';                        // Kırmızı (negatif)
+        // İSTEDİĞİN RENK MANTIĞI BURADA
+        colors: [function({ value }) {
+            if (value >= 3) return '#064e3b';      // Koyu Yeşil
+            if (value > 0) return '#10b981';       // Açık Yeşil
+            if (value <= -3) return '#7f1d1d';     // Koyu Kırmızı
+            if (value < 0) return '#ef4444';       // Açık Kırmızı
+            return '#4b5563';                      // Nötr (0)
         }],
         plotOptions: {
             treemap: {
@@ -78,189 +45,60 @@ function upd() {
         },
         dataLabels: {
             enabled: true,
-            style: { 
-                fontSize: '20px',  // Daha büyük yazı
-                fontWeight: '900',
-                fontFamily: 'Inter, sans-serif',
-                colors: ['#ffffff']
-            },
+            style: { fontSize: '14px', fontWeight: 'bold' },
             formatter: (text, op) => [text, op.value + "%"]
         },
-        theme: { mode: 'dark' },
-        tooltip: { 
-            enabled: false  // TOOLTIP KAPALI
-        }
+        tooltip: { theme: 'dark' }
     };
 
-    document.querySelector("#chart").innerHTML = "";
-    new ApexCharts(document.querySelector("#chart"), o).render();
+    const chartDiv = document.querySelector("#chart");
+    chartDiv.innerHTML = "";
+    const chart = new ApexCharts(chartDiv, options);
+    chart.render();
 }
 
 async function openDetail(stock) {
-    currentStock = stock;
-    document.getElementById('detailTitle').textContent = stock;
+    document.getElementById('detailTitle').innerText = stock + " Detay Grafiği";
     document.getElementById('detailModal').style.display = 'flex';
-    document.getElementById('detailOv').style.display = 'block';
-    
-    await loadDetailChart();
+    document.getElementById('ov').style.display = 'block';
+
+    const symbol = stock + ".IS";
+    // Yahoo Finance CORS hatasını aşmak için (Sadece candlestick verisi için proxy kullanımı)
+    // Eğer veri yüklenmiyorsa tarayıcıda geçici olarak CORS izinlerini açmak gerekebilir 
+    // veya Python ile bu veriyi önceden çekmek en sağlıklısıdır.
+    try {
+        const response = await fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?range=1d&interval=5m`);
+        const data = await response.json();
+        const res = data.chart.result[0];
+        const seriesData = res.timestamp.map((t, i) => ({
+            x: new Date(t * 1000),
+            y: [res.indicators.quote[0].open[i], res.indicators.quote[0].high[i], res.indicators.quote[0].low[i], res.indicators.quote[0].close[i]]
+        })).filter(d => d.y[3] != null);
+
+        const options = {
+            series: [{ data: seriesData }],
+            chart: { type: 'candlestick', height: '100%', theme: 'dark' },
+            xaxis: { type: 'datetime' }
+        };
+
+        const detailDiv = document.querySelector("#detailChart");
+        detailDiv.innerHTML = "";
+        new ApexCharts(detailDiv, options).render();
+    } catch (e) {
+        document.getElementById('detailChart').innerHTML = "<p style='text-align:center; padding-top:50px;'>Veri çekilemedi. (Tarayıcı CORS engeli veya Yahoo API kısıtlaması)</p>";
+    }
 }
 
 function closeDetail() {
     document.getElementById('detailModal').style.display = 'none';
-    document.getElementById('detailOv').style.display = 'none';
+    document.getElementById('ov').style.display = 'none';
 }
 
-async function changeTime(period) {
-    currentTime = period;
-    
-    ['1d', '1mo', '6mo', '1y', '5y'].forEach(p => {
-        document.getElementById('t-' + p).classList.toggle('active', p === period);
-    });
-    
-    await loadDetailChart();
-}
-
-async function loadDetailChart() {
-    try {
-        const symbol = currentStock + ".IS";
-        const response = await fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?range=${currentTime}&interval=${currentTime === '1d' ? '5m' : '1d'}`);
-        const data = await response.json();
-        
-        if (!data.chart || !data.chart.result || !data.chart.result[0]) {
-            throw new Error('Veri yok');
-        }
-        
-        const timestamps = data.chart.result[0].timestamp;
-        const prices = data.chart.result[0].indicators.quote[0];
-        
-        const series = timestamps.map((t, i) => ({
-            x: new Date(t * 1000),
-            y: [prices.open[i], prices.high[i], prices.low[i], prices.close[i]]
-        })).filter(s => s.y.every(v => v !== null && v !== undefined));
-        
-        const options = {
-            series: [{
-                name: currentStock,
-                data: series
-            }],
-            chart: {
-                type: 'candlestick',
-                height: 500,
-                background: 'transparent',
-                toolbar: { show: true }
-            },
-            plotOptions: {
-                candlestick: {
-                    colors: {
-                        upward: '#16c784',
-                        downward: '#ea3943'
-                    }
-                }
-            },
-            xaxis: {
-                type: 'datetime',
-                labels: { style: { colors: '#9ca3af' } }
-            },
-            yaxis: {
-                tooltip: { enabled: true },
-                labels: { 
-                    style: { colors: '#9ca3af' },
-                    formatter: (val) => val.toFixed(2)
-                }
-            },
-            grid: {
-                borderColor: '#1e2639'
-            },
-            theme: { mode: 'dark' }
-        };
-        
-        document.getElementById('detailChart').innerHTML = '';
-        detailChartObj = new ApexCharts(document.querySelector("#detailChart"), options);
-        detailChartObj.render();
-        
-    } catch (e) {
-        console.error("Grafik yüklenemedi:", e);
-        document.getElementById('detailChart').innerHTML = '<p style="text-align:center; padding:50px; color:#9ca3af;">Veri yüklenemedi. Lütfen tekrar deneyin.</p>';
-    }
-}
-
-function openM() { 
-    document.getElementById('modal').style.display = 'flex'; 
-    document.getElementById('ov').style.display = 'block'; 
-}
-
-function closeM() { 
-    document.getElementById('modal').style.display = 'none'; 
-    document.getElementById('ov').style.display = 'none'; 
-}
-
-function list() {
-    const l = document.getElementById('stock-list');
-    let html = '';
-    
-    allStocks.forEach(sector => {
-        html += `<div class="sector-group">`;
-        html += `<div class="sector-title">${sector.name}</div>`;
-        
-        sector.data.forEach(h => {
-            html += `
-                <div class="stock-row" onclick="toggle('${h.x}')">
-                    <div class="stock-info">
-                        <span class="stock-code">${h.x}</span>
-                        <span class="stock-sector">${sector.name}</span>
-                    </div>
-                    <input type="checkbox" 
-                           id="chk-${h.x}" 
-                           data-sector="${sector.name}"
-                           ${sel.includes(h.x) ? 'checked' : ''} 
-                           onclick="event.stopPropagation()">
-                </div>
-            `;
-        });
-        
-        html += `</div>`;
-    });
-    
-    l.innerHTML = html;
-}
-
-function toggle(id) {
-    const c = document.getElementById('chk-' + id);
-    c.checked = !c.checked;
-}
-
-function fltr() {
-    let q = document.getElementById('srch').value.toUpperCase();
-    
-    document.querySelectorAll('.sector-group').forEach(group => {
-        let hasVisibleStock = false;
-        
-        group.querySelectorAll('.stock-row').forEach(row => {
-            const code = row.querySelector('.stock-code').textContent;
-            const sector = row.querySelector('.stock-sector').textContent;
-            const matches = code.includes(q) || sector.toUpperCase().includes(q);
-            
-            row.style.display = matches ? 'flex' : 'none';
-            if (matches) hasVisibleStock = true;
-        });
-        
-        group.style.display = hasVisibleStock ? 'block' : 'none';
-    });
-}
-
-function save() {
-    sel = Array.from(document.querySelectorAll('#stock-list input:checked'))
-           .map(c => c.id.replace('chk-', ''));
-    localStorage.setItem('sel', JSON.stringify(sel));
-    closeM();
-    if (view === 'mine') upd();
-}
-
-function changeV(v) {
-    view = v;
-    document.getElementById('b-bist100').classList.toggle('active', v === 'bist100');
-    document.getElementById('b-mine').classList.toggle('active', v === 'mine');
-    upd();
+function changeView(v) {
+    currentView = v;
+    document.getElementById('btn-bist100').className = v === 'bist100' ? 'active' : '';
+    document.getElementById('btn-mine').className = v === 'mine' ? 'active' : '';
+    renderChart();
 }
 
 init();
